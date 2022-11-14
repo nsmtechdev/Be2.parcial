a113cfd3973d8f3b708d4a0e7063c982ac729407
nsmtechdev <nsmtechdev@gmail.com>
2022-11-14 15:17:17 -0300

    test1

--- api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
+++ api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
@@ -5,12 +5,14 @@ import com.dh.movie.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class ApiMovieApplication {
 
     public static void main(String[] args) {
